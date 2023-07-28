3faf17396c7769d6511a4f23e746a8a2d37c6cce
jakub_ant <jakub.antosiewicz.1998@gmail.com>
2023-07-28 21:26:19 +0200

    Handle mobile navigation

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ const sections = sectionsIds.reduce(
   }),
   {}
 );
-sectionsIds.forEach((selector) => {
-  document.querySelector(`.${selector}`).addEventListener("click", () => {
-    console.log("efsefsdf");
-    sections[selector].scrollIntoView({
-      behavior: "smooth",
-      inline: "start",
-      block: "center",
-    });
-  });
-});
 
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const asideMenu = document.querySelector(".aside-menu");
@@ -25,3 +15,35 @@ hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("hamburger-menu-open");
   asideMenu.classList.toggle("aside-menu-open");
 });
+
+const scrollToSelectedSection = (selector) => {
+  sections[selector].scrollIntoView({
+    behavior: "smooth",
+    inline: "start",
+    block: "center",
+  });
+};
+
+const scrollToSelectedSectionAndCloseHamburgerMenu = (selector) => {
+  scrollToSelectedSection(selector);
+  hamburgerMenu.classList.remove("hamburger-menu-open");
+  asideMenu.classList.remove("aside-menu-open");
+};
+
+const addEventListenerToNavElemenr = ({ element, selector }) => {
+  const isMobile = element.dataset.device === "mobile";
+  const handler = isMobile
+    ? scrollToSelectedSectionAndCloseHamburgerMenu
+    : scrollToSelectedSection;
+
+  const callback = () => {
+    handler(selector);
+  };
+  element.addEventListener("click", callback);
+};
+
+for (const selector of sectionsIds) {
+  document.querySelectorAll(`.${selector}`).forEach((element) => {
+    addEventListenerToNavElemenr({ selector, element });
+  });
+}
